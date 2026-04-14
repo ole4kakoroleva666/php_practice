5c6230bb0145792b47a64be9aa8a71f2827ed7e4
gr432_zeoyur <zezulaolga9@gmail.com>
2026-04-14 22:55:52 +0700

    before registration

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -1,15 +1,22 @@
 <?php
 namespace Controller;
 
+use Model\Post;
 use Src\View;
+use Src\Request;
 
-class Site {
-    public function index() : string{
-        $view = new View();
-        return $view->render('site.hello', ['message' => 'index working']);
-    }
+class Site
+{
+public function index(Request $request): string
+{
+ $posts = Post::where('id', $request->id)->get();
+ return (new View())->render('site.post', ['posts' => $posts]);
+}
 
-    public function hello() : string {
-        return new View('site.hello', ['message' => 'hello working']);
+
+    public function hello(): string
+    {
+        return new View('site.hello', ['message' => 'hello
+        working']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@
 нужно вызвать).*/
 
 use Src\Route;
-
+Route::add('', [Controller\Site::class, 'index']);
 Route::add('go', [Controller\Site::class, 'index']);
 Route::add('hello', [Controller\Site::class, 'hello']);
